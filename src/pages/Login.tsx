770e194250639f346f7cd17e1308c62c9f6180e8
ravindrap52 <ravindrapallikonda@gmail.com>
2024-07-27 12:59:23 +0200

    feat(form-validation): implements form validation using react-hook form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,35 @@
 import { Box, Button, TextField, Typography } from "@mui/material";
+import { red } from "@mui/material/colors";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+// form schema
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string().min(4),
+});
+
+type LoginForm = z.infer<typeof schema>;
 
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginForm>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    resolver: zodResolver(schema),
+  });
+
+  // submitting the form
+  const submitLoginForm: SubmitHandler<LoginForm> = (data) => {
+    console.log(data);
+  };
+
   return (
     <Box
       sx={{
@@ -8,39 +37,47 @@ export default function Login() {
         flexDirection: "column",
         alignItems: "center",
         border: "1px solid rgba(0, 0, 0, 0.23)",
-        borderRadius:" 0.3rem",
+        borderRadius: " 0.3rem",
         padding: "0.5rem",
       }}
     >
       <Typography component="h1" variant="h5">
         Login
       </Typography>
-      <Box component="form" noValidate>
+      <Box component="form" width="md" noValidate onSubmit={handleSubmit(submitLoginForm)}>
         <TextField
           margin="normal"
-          required
           fullWidth
           id="email"
           label="Email"
-          name="email"
           autoFocus
+          {...register("email")}
         />
+        {errors.email && (
+          <Typography component="p" color={red[400]}>
+            {errors.email.message}
+          </Typography>
+        )}
         <TextField
           margin="normal"
-          required
           fullWidth
-          name="password"
           label="Password"
           type="password"
           id="password"
+          {...register("password")}
         />
+        {errors.password && (
+          <Typography component="p" color={red[400]}>
+            {errors.password.message}
+          </Typography>
+        )}
         <Button
           type="submit"
           fullWidth
           variant="contained"
           sx={{ mt: 2, mb: 2 }}
         >
-          Login
+          {isSubmitting ? "Submitting..." : "Login"}
         </Button>
       </Box>
     </Box>
